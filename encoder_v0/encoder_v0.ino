29b342174bef0cdfe82fbf1eccf6866d2427ae88
cassiofm <cassiofm@bol.com.br>
2021-07-01 14:50:54 -0300

    versao 1.0 do programa encoder

---
+++ encoder_v0/encoder_v0.ino
@@ -0,0 +1,55 @@
+volatile  int temp, counter = 0; //This variable will increase or decrease depending on the rotation of encoder
+ bool flag= LOW;
+ int chave = 7;   
+void setup() {
+  Serial.begin (9600);
+ pinMode(LED_BUILTIN, OUTPUT);
+ pinMode(chave, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP); // internal pullup input pin 2 
+  
+  pinMode(3, INPUT_PULLUP); // internalเป็น pullup input pin 3
+//Setting up interrupt
+  //A rising pulse from encodenren activated ai0(). AttachInterrupt 0 is DigitalPin nr 2 on moust Arduino.
+  attachInterrupt(0, ai0, RISING);
+   
+  //B rising pulse from encodenren activated ai1(). AttachInterrupt 1 is DigitalPin nr 3 on moust Arduino.
+  attachInterrupt(1, ai1, RISING);
+  }
+   
+  void loop() {
+  // Send the value of counter
+  if( counter != temp ){
+  Serial.println (counter);
+  temp = counter;
+  }
+  if( counter > 500){
+    flag = HIGH;
+    counter = 0;
+  } 
+ 
+ if(flag && digitalRead(chave)){
+  flag = LOW;
+ }
+  digitalWrite(LED_BUILTIN, flag);
+  
+  }
+   
+  void ai0() {
+  // ai0 is activated if DigitalPin nr 2 is going from LOW to HIGH
+  // Check pin 3 to determine the direction
+  if(digitalRead(3)==LOW) {
+  counter++;
+  }else{
+  counter--;
+  }
+  }
+   
+  void ai1() {
+  // ai0 is activated if DigitalPin nr 3 is going from LOW to HIGH
+  // Check with pin 2 to determine the direction
+  if(digitalRead(2)==LOW) {
+  counter--;
+  }else{
+  counter++;
+  }
+  }
